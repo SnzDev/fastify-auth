72603520dfa79266f65e18b110ab2f94a5b45850
SnzDev <tms.working@gmail.com>
2025-02-23 21:21:42 -0300

    chore: Remove unused code in user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,5 @@
 import { FastifyTypedInstance } from '../@types/fastify-typed-instance';
 import { userMiddleware } from '../middlewares/user-middleware';
-import { me } from './auth/me';
-import { refreshToken } from './auth/refresh-token';
 
 export function userRoutes(app: FastifyTypedInstance) {
   app.addHook('preHandler', (req, reply) => userMiddleware(req, reply, app));
